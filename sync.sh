c1d4dd5f2ddd742bea182a754f2ccd6509405875
ArhamWaliullahYunusAnsari <ansariarham1952@outlook.com>
2026-06-15 09:50:57 +0530

    updated manifest and sync path

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-BASE_PATH="${1:-./DemoVault}"
+BASE_PATH="${1:-./Users/arhamansari/Library/Mobile Documents/iCloud~md~obsidian/Documents/Vault X Notes}"
 yarn build
 rm -rf "$BASE_PATH/.obsidian/plugins/obsidian-plugin-todo/"
 mkdir -p "$BASE_PATH/.obsidian/plugins/obsidian-plugin-todo/"
-cp main.js manifest.json styles.css "$BASE_PATH/.obsidian/plugins/obsidian-plugin-todo/"
+cp main.js manifest.json styles.css "$BASE_PATH/.obsidian/plugins/TO-DO-obsidian/"
